67571f7a5721bdeeb9d99b09a65dbac753b1198b
Grigory <grial00.06@gmail.com>
2022-06-19 13:07:47 +0300

    camera control mod working, but camera is shaking when angle 10+

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,13 +8,19 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
     public float cameraSpeed;
     public Transform cameraCenterTransform;
-    public int cameraMod = 1; //0 - camera is controlled by the player, 1 - camera is controlled by the mouse  
-    public float sensitivity = 100; 
+    public int cameraMod = 0; //0 - camera is controlled by the player, 1 - camera is controlled by the mouse  
+    public float sensitivity = 100;
+    public Vector2 minMaxAngleY = Vector2.one;
+    public float maxAngleX = 180;
+    public Vector3 centerCamraMod2 = new Vector3(0, 1, 0);
+
+    private Quaternion rotationX, rotationY;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = offset;
+        SetStartPositionCameraMod(cameraMod);
     }
 
     // Update is called once per frame
@@ -23,27 +29,63 @@ public class CameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.M))
         {
             cameraMod = (cameraMod + 1) % 2;
+            SetStartPositionCameraMod(cameraMod);
         }
 
         switch (cameraMod)
         {
             case 0:
-                cameraCenterTransform.position = playerTransform.localPosition;
-                cameraCenterTransform.eulerAngles = new Vector3(0, playerTransform.eulerAngles.y, 0);
+                cameraCenterTransform.position = playerTransform.position;
+                cameraCenterTransform.rotation = Quaternion.AngleAxis(playerTransform.eulerAngles.y, Vector3.up);
                 break;
 
             case 1:
-                cameraCenterTransform.position = playerTransform.localPosition;
+                cameraCenterTransform.position = playerTransform.position + centerCamraMod2;
                 Vector3 MousePos = Input.mousePosition;
-                float myAngle = (MousePos.x * 2 / Screen.width) * 90 - 35;
-                //cameraCenterTransform.transform.RotateAround(cameraCenterTransform.position, Vector3.up, myAngle);
-                myAngle = (MousePos.y * 2 / Screen.height) * 90 - 35;
-                cameraCenterTransform.transform.rotation = Quaternion.AngleAxis(myAngle, cameraCenterTransform.right);
+
+                float myAngle = (MousePos.x / Screen.width) * (2 * maxAngleX) - maxAngleX;
+                rotationX = Quaternion.AngleAxis(myAngle, Vector3.up);
+
+                
+                myAngle = (MousePos.y / Screen.height) * (minMaxAngleY.x + minMaxAngleY.y) + minMaxAngleY.x;
+                rotationY = (myAngle <= minMaxAngleY.y && myAngle >= minMaxAngleY.x)
+                    ? Quaternion.AngleAxis(myAngle, cameraCenterTransform.right) 
+                    : (myAngle > minMaxAngleY.y) 
+                    ? Quaternion.AngleAxis(minMaxAngleY.y, cameraCenterTransform.right)
+                    : Quaternion.AngleAxis(minMaxAngleY.x, cameraCenterTransform.right);
+
+                cameraCenterTransform.rotation = (rotationY.normalized * rotationX.normalized).normalized;
+
                 break;
 
             default:
                 cameraCenterTransform.position = playerTransform.localPosition;
                 cameraCenterTransform.eulerAngles = new Vector3(0, playerTransform.eulerAngles.y, 0);
+                break;
+        }
+    }
+
+    void SetStartPositionCameraMod(int cameraMod)
+    {
+        switch (cameraMod)
+        {
+            case 0:
+                cameraCenterTransform.position = playerTransform.position;
+                transform.localPosition = offset;
+                transform.localRotation = Quaternion.AngleAxis(25, transform.right);
+                break;
+
+            case 1:
+                cameraCenterTransform.position = playerTransform.position + centerCamraMod2;
+                transform.localPosition = new Vector3(0.0f, 0.0f, offset.z);
+                transform.localRotation = Quaternion.identity;
+                Cursor.lockState = CursorLockMode.None;
+                break;
+
+            default:
+                cameraCenterTransform.position = playerTransform.position;
+                transform.localPosition = offset;
+                transform.localRotation = Quaternion.AngleAxis(25, transform.right);
                 break;
         }
     }
