ec4f6ee9750cc1e215b9b364456d070d418f8e7d
Grigory <grial00.06@gmail.com>
2022-06-19 21:44:38 +0300

    added crouching and settings capsule coliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,31 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheckerTransform;
     public LayerMask notPlayerMask;
     public float jumpForce = 2.0f;
+    [Space]
+    [Header("Stay Capsule Collider")]
+    public Vector3 centerStayCollider;
+    public float radiusStayCollider;
+    public float heightStayCollider;
+    [Space]
+    [Header("Crouch Capsule Collider")]
+    public Vector3 centerCrouchCollider;
+    public float radiusCrouchCollider;
+    public float heightCrouchCollider;
 
     private Animator animator;
     new private Rigidbody rigidbody;
     private Vector3 direction;
+    private float crouchSpeedKef = 1.0f;
+    private CapsuleCollider capsuleCollider;
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        SetCapsuleCollider("Stay");
     }
 
     // Update is called once per frame
@@ -37,13 +52,53 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && FindGroundRaycast(0.1f))
+        {
+            SetCapsuleCollider("Crouch");
+            crouchSpeedKef = 0.4f;
+            animator.SetBool("Crouch", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            SetCapsuleCollider("Stay");
+            crouchSpeedKef = 1.0f;
+            animator.SetBool("Crouch", false);
+        }
+
         if (Physics.CheckSphere(groundCheckerTransform.position, 0.17f, notPlayerMask))
         {
             animator.SetBool("IsInAir", false);
+            isGrounded = true;
         }
         else
         {
             animator.SetBool("IsInAir", true);
+            isGrounded = true;
+        }
+    }
+
+    private void SetCapsuleCollider(string type)
+    {
+        switch (type)
+        {
+            case "Stay":
+                capsuleCollider.center = centerStayCollider;
+                capsuleCollider.radius = radiusStayCollider;
+                capsuleCollider.height = heightStayCollider;
+                break;
+
+            case "Crouch":
+                capsuleCollider.center = centerCrouchCollider;
+                capsuleCollider.radius = radiusCrouchCollider;
+                capsuleCollider.height = heightCrouchCollider;
+                break;
+
+            default:
+                capsuleCollider.center = centerStayCollider;
+                capsuleCollider.radius = radiusStayCollider;
+                capsuleCollider.height = heightStayCollider;
+                break;
         }
     }
 
@@ -54,7 +109,8 @@ public class PlayerController : MonoBehaviour
         direction = Vector3.ClampMagnitude(transform.rotation * (new Vector3(0, 0, Input.GetAxis("Vertical"))), 1);
 
         //rigidbody.velocity = new Vector3(direction.x * speed, rigidbody.velocity.y, direction.z * speed);
-        rigidbody.velocity = new Vector3(direction.x * speed, rigidbody.velocity.y, direction.z * speed);
+        float resultSpeed = speed * crouchSpeedKef;
+        rigidbody.velocity = new Vector3(direction.x * resultSpeed, rigidbody.velocity.y, direction.z * resultSpeed);
         rigidbody.angularVelocity = Vector3.zero;
         animator.SetFloat("Speed", (Input.GetAxis("Vertical")<0?-1:1) * direction.magnitude);
     }
@@ -79,10 +135,12 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (Physics.Raycast(groundCheckerTransform.position, Vector3.down, 0.1f, notPlayerMask))
+        if (!animator.GetBool("Crouch") && isGrounded)
         {
             animator.SetTrigger("Jumping");
             rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
+
+    private bool FindGroundRaycast(float dist) => (Physics.Raycast(groundCheckerTransform.position, Vector3.down, dist, notPlayerMask));
 }
